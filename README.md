baa1cef64ea6ef04e627753be80790f6f352c3c0
KitKatK8 <kaitlynhemphill1990@gmail.com>
2022-09-25 15:23:58 -0400

    adding js

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ TOP contact page project
 2-3 hrs setup
 
 1 hr adjusting logo and placement
+
+
+1:34pm ,,,,lets see how long it takes to add js for password match detector.
+1:50pm break....made basic function to return false but need to return css falso styling elements and such.
+3:02 pm wow...i simply didn't have the script tag in my html.
+
+3:20 still not working after script link insert. 
--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 
+//password match detection. lets start with caching the dom elements necessary here. being: user input in password and confirm password fields.
+
+let form = document.querySelector("form");
+
+function matchDetection() {
+    console.log("this works");
+    let button = document.getElementsByClassName(".submitButton");
+    let passwordInput = document.getElementByid(".password").value;
+    let confirmInput = document.getElementByid(".confirmPassword").value;
+    let message = document.querySelector(".message");
+
+    button.addEventListener('submit', () => {
+        console.log("test");
+         message.textContent = "Passwords need to match.";
+    });
+
+    console.log(" Password:", passwordInput,'\n',"Confirm Password:",confirmPassword);
+*/
+   /* if(passwordInput.length != confirmInput.length){
+        return message.textContent = "Passwords need to match.";
+    }; */
+
+}
+
